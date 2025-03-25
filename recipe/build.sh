20cd52b1526a944f103ac8efa60efea079c75006
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2025-03-25 23:54:57 +0100

    Use --with-cfitsio

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,8 +4,10 @@
 # Putting the lib path directly in LD_LIBRARY_PATH does the trick,
 # and is actually recommended by the CPL documentation,
 # https://ftp.eso.org/pub/dfs/pipelines/libraries/cpl/docs/cpl-user-manual_6.0.pdf
+#
+# For some reason it is necessary to specify --with-cfitsio since cfitsio 4.6.0.
 env LD_LIBRARY_PATH="${PREFIX}/lib:${LD_LIBRARY_PATH}" \
-  ./configure --prefix=${PREFIX}
+  ./configure --prefix=${PREFIX} --with-cfitsio=${PREFIX}
 make
 make check
 make install
